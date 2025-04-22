76b43c18171d4b67653720786526695c7e3a62c0
Rosa Turner <rmturner1220@gmail.com>
2025-04-21 22:13:28 -0400

    add backend tweaks for login/user improvements

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["id", "username", "password", 'first_name', 'last_name', 'email', 'is_staff']
         extra_kwargs = { 
             "password" : { 
                 "write_only" : True 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import CurrentUserView
+from .views import home
 
 urlpatterns = [
     path("api/user/me/", CurrentUserView.as_view(), name="current_user"),
+    path('', home),
 ]
