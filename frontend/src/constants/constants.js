62aaefc5e7e2a7f5b62c2394dd951f397a92f1e1
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2025-04-26 20:26:25 -0400

    Update constants.js

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -2,4 +2,4 @@ export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh";
 export const FORM_METHOD_LOGIN = "login";
 export const FORM_METHOD_REGISTER = "register";
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = "http://127.0.0.1:8000" // TODO: Figure out why this doesn't work on Windows: import.meta.env.VITE_API_URL;
